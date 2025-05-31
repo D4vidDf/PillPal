07f15b9dea191175ed843f52319dc9c29f7fb8ed
david <dominguezfondodavid@gmail.com>
2025-05-31 15:09:17 +0200

    updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.d4viddf.medicationreminder"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.2.1"
+        versionCode = 13
+        versionName = "0.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
