f269d6b2c3205c729abfb88eb95afda1726d6097
david <dominguezfondodavid@gmail.com>
2025-07-14 18:49:37 +0200

    Updated design to use edgebutton, timetext and added missing dependencies

--- wear/src/main/java/com/d4viddf/medicationreminder/wear/presentation/components/RemindersContent.kt
+++ wear/src/main/java/com/d4viddf/medicationreminder/wear/presentation/components/RemindersContent.kt
@@ -3,25 +3,44 @@ package com.d4viddf.medicationreminder.wear.presentation.components
 import android.app.Application
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
+import androidx.wear.compose.foundation.lazy.TransformingLazyColumn
+import androidx.wear.compose.foundation.lazy.rememberTransformingLazyColumnState
+import androidx.wear.compose.material3.Button
+import androidx.wear.compose.material3.EdgeButton
+import androidx.wear.compose.material3.EdgeButtonSize
+import androidx.wear.compose.material3.FilledIconButton
+import androidx.wear.compose.material3.Icon
+import androidx.wear.compose.material3.ListHeader
 import androidx.wear.compose.material3.MaterialTheme
+import androidx.wear.compose.material3.ScreenScaffold
+import androidx.wear.compose.material3.SurfaceTransformation
 import androidx.wear.compose.material3.Text
+import androidx.wear.compose.material3.TimeText
+import androidx.wear.compose.material3.lazy.rememberTransformationSpec
+import androidx.wear.compose.material3.lazy.transformedHeight
 import androidx.wear.tooling.preview.devices.WearDevices
 import com.d4viddf.medicationreminder.wear.R
 import com.d4viddf.medicationreminder.wear.data.WearReminder
 import com.d4viddf.medicationreminder.wear.presentation.theme.MedicationReminderTheme
+import com.google.android.horologist.compose.layout.rememberResponsiveColumnPadding
 // Import the newly created MedicationListItem
 import com.d4viddf.medicationreminder.wear.presentation.components.MedicationListItem
+import com.google.android.horologist.compose.layout.ColumnItemType
 
 @Composable
 fun RemindersContent(
@@ -29,43 +48,58 @@ fun RemindersContent(
     onMarkAsTaken: (WearReminder) -> Unit,
     onMoreClick: () -> Unit
 ) {
-    if (reminders.isEmpty()) {
-        Box(modifier = Modifier.fillMaxSize().padding(16.dp), contentAlignment = Alignment.Center) {
-            Text(
-                text = stringResource(R.string.no_reminders_today),
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.onBackground,
-                textAlign = TextAlign.Center
-            )
-        }
-        return
-    }
 
-    ScalingLazyColumn(
-        modifier = Modifier.fillMaxSize().padding(vertical = 16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        item {
-            Text(
-                text = stringResource(R.string.todays_reminders_header),
-                style = MaterialTheme.typography.titleMedium,
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
-        }
-        items(reminders.size) { index ->
-            val reminder = reminders[index]
-            MedicationListItem(
-                reminder = reminder,
-                onMarkAsTaken = { onMarkAsTaken(reminder) },
-                modifier = Modifier.padding(horizontal = 16.dp)
-            )
-        }
-        item {
-            Button(
+    val listState = rememberTransformingLazyColumnState()
+    val transformationSpec = rememberTransformationSpec()
+    ScreenScaffold(
+        scrollState = listState,
+        contentPadding =  rememberResponsiveColumnPadding(
+            first = ColumnItemType.ListHeader,
+            last = ColumnItemType.IconButton
+        ),
+        edgeButton = {
+            EdgeButton(
                 onClick = onMoreClick,
-                modifier = Modifier.padding(top = 8.dp)
+                buttonSize = EdgeButtonSize.ExtraSmall
             ) {
-                Text(text = stringResource(R.string.more_options))
+                Text(stringResource(R.string.more_options), textAlign = TextAlign.Center)
+            }
+        },
+        timeText = { TimeText() }
+    ) { contentPadding ->
+        TransformingLazyColumn (
+            state = listState,
+            contentPadding = contentPadding,
+        ) {
+            item {
+                ListHeader(
+                    modifier =
+                        Modifier.fillMaxWidth().transformedHeight(this, transformationSpec),
+                    transformation = SurfaceTransformation(transformationSpec)
+                ) {
+                    Text(
+                        text = stringResource(R.string.todays_reminders_header),
+                    )
+                }
+            }
+            items(reminders.size) { index ->
+                val reminder = reminders[index]
+                MedicationListItem(
+                    reminder = reminder,
+                    onMarkAsTaken = { onMarkAsTaken(reminder) },
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+            }
+            item{
+                if (reminders.isEmpty()) {
+
+                        Text(
+                            text = stringResource(R.string.no_reminders_today),
+                            style = MaterialTheme.typography.bodyLarge,
+                            color = MaterialTheme.colorScheme.onBackground,
+                            textAlign = TextAlign.Center
+                        )
+                }
             }
         }
     }
@@ -80,7 +114,10 @@ fun PreviewRemindersContentWithData() {
         WearReminder("rem3",3, 3L, 103L, "Aspirin", "12:00", false, "81mg", null)
     )
     MedicationReminderTheme {
-        RemindersContent(reminders = sampleReminders, onMarkAsTaken = {})
+        RemindersContent(
+            reminders = sampleReminders, onMarkAsTaken = {},
+            onMoreClick = {}
+        )
     }
 }
 
@@ -88,6 +125,9 @@ fun PreviewRemindersContentWithData() {
 @Composable
 fun PreviewRemindersContentEmpty() {
     MedicationReminderTheme {
-        RemindersContent(reminders = emptyList(), onMarkAsTaken = {})
+        RemindersContent(
+            reminders = emptyList(), onMarkAsTaken = {},
+            onMoreClick = {}
+        )
     }
 }
