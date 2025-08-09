d1eff69568dba49aa1667f94b60c0dcfdaf23e8c
david <dominguezfondodavid@gmail.com>
2025-08-09 22:02:09 +0200

    First steps for loading skeleton

--- app/src/main/java/com/d4viddf/medicationreminder/ui/features/medication/vault/MedicationVaultViewModel.kt
+++ app/src/main/java/com/d4viddf/medicationreminder/ui/features/medication/vault/MedicationVaultViewModel.kt
@@ -23,6 +23,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
