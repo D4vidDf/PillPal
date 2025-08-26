0a8e7e12b4fe1d78bc0926dd073dbb7efcdc6396
david <dominguezfondodavid@gmail.com>
2025-08-26 21:57:48 +0200

    Updated water goal

--- app/src/main/java/com/d4viddf/medicationreminder/ui/features/settings/NutritionWeightSettingsScreen.kt
+++ app/src/main/java/com/d4viddf/medicationreminder/ui/features/settings/NutritionWeightSettingsScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
--- app/src/main/java/com/d4viddf/medicationreminder/ui/features/settings/WaterIntakeGoalScreen.kt
+++ app/src/main/java/com/d4viddf/medicationreminder/ui/features/settings/WaterIntakeGoalScreen.kt
@@ -21,7 +21,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
